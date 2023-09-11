248be028b9b427566a3b048ec110013dcccd8230
imranture <imranture@gmail.com>
2023-09-11 17:39:13 +0300

    Update detect touch function

--- assets/js/detect-touch.js
+++ assets/js/detect-touch.js
@@ -1,7 +1,32 @@
+// Function to detect touch devices and add 'no-touch' class
 const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 const isSmallScreen = window.innerWidth <= 960;
 
 if (!isTouchDevice || (isTouchDevice && !isSmallScreen)) {
   // Add 'no-touch' to any element to indicate it's not a touch device
   document.body.classList.add('no-touch');
+}
+
+// Flag to indicate if email has been copied
+let emailCopied = false;
+
+// Function to set the emailCopied flag to true
+function setEmailCopiedTrue() {
+  emailCopied = true;
+}
+
+// Function to remove hover effect on touch devices after email is copied
+if (isTouchDevice) {
+  const emailLink = document.getElementById('emailLink');
+
+  emailLink.addEventListener('touchstart', function () {
+    if (emailCopied) {
+      // Remove hover effect after 6 seconds (1500ms + 4500ms)
+      setTimeout(() => {
+        emailLink.blur();
+        // Reset the flag
+        emailCopied = false;
+      }, 6000);
+    }
+  });
 }
